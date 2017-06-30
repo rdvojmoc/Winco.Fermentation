a9b117989552546cf402a58984784cf87dabd921
rdvojmoc <rok.dvojmoc@gmail.com>
2017-07-01 00:33:46 +0200

    Added load of json configurations, changed measurement interval from 30 to 20 seconds.

--- main.py
+++ main.py
@@ -2,6 +2,10 @@ import serial
 import sys, os, getopt, time, signal, json
 import losantHelper
 
+dirName = os.path.dirname(os.path.abspath(__file__))
+secrets = {}
+settings = {}
+
 # initialize the serial port
 serialPort = serial.Serial('/dev/ttyS1', 9600, timeout=2)
 if serialPort.isOpen() == False:
@@ -22,25 +26,42 @@ def signalHandler(signal, frame):
 	endMeasurements()
 	sys.exit()
 
+def loadJSONConfig(filePath):
+	configuration = {};
+	with open( filePath ) as f:
+		try:
+			configuration = json.load(f)
+		except:
+			print("ERROR: expecting JSON file")
+			sys.exit()
+	return configuration
+	
 # define a signal to run a function when ctrl+c is pressed
 signal.signal(signal.SIGINT, signalHandler)
 
 def mainProgram():
 	print("Starting wine fermentation data collection...");
 	
-	losantHelper.init("", "", "")
+	secrets = loadJSONConfig('/'.join([dirName, "secrets.json"]))
+	settings = loadJSONConfig('/'.join([dirName, "settings.json"]))
 
-	print("Initialization done.");
+	losantHelper.init(secrets["deviceId"], secrets["key"], secrets["secret"])
+	
+	print("Initialization done.")
 
 	while True:
 		# get a reading from the plant
 		serialPort.write('r')
+		
 		state = {"OutsideTemperature": serialPort.readline(), "WellWaterTemperature": serialPort.readline()}
 		
 		losantHelper.sendMeasurement(state)
 		
 		# delay between readings
-		time.sleep(30)
+		time.sleep(20)
+	
+	#close serial
+	endMeasurements()
 
 if __name__ == "__main__":
 	mainProgram()
